672e3c1ea22265fa314269934097630fa6d4caab
Jason <23126557+dyckia@users.noreply.github.com>
2020-04-07 15:55:03 -0230

    Update test_image to comply with new API

--- test_image.py
+++ test_image.py
@@ -20,8 +20,10 @@ def test_single_image(lr_image, upscale_factor):
         model.load_state_dict(torch.load('epochs/' + model_name))
     else:
         model.load_state_dict(torch.load('epochs/' + model_name, map_location=lambda storage, loc: storage))
-
-    image = Variable(ToTensor()(lr_image), volatile=True).unsqueeze(0)
+    # volatile is no longer avaiable
+    # image = Variable(ToTensor()(lr_image), volatile=True).unsqueeze(0)
+    with torch.no_grad():
+        image = Variable(ToTensor()(lr_image)).unsqueeze(0)
     if test_mode:
         image = image.cuda()
 
